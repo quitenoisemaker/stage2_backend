c741890cd720ae584d7a0da2797bd93dcac532d1
samson <samsonojugo@gmail.com>
2021-08-18 11:27:38 +0100

    add php mailer update3

--- index.php
+++ index.php
@@ -213,8 +213,8 @@ try {
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'samsonojugo@gmail.com';                     //SMTP username
     $mail->Password   = 'quietnoisemaker';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('samsonojugo@gmail.com', 'Mailer');
