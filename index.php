1d03db1b77b29ab47abdc5c8609955a7987ee2e6
samson <samsonojugo@gmail.com>
2022-08-11 07:55:14 +0100

    minor update

--- index.php
+++ index.php
@@ -20,19 +20,20 @@
 </head>
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Inconsolata:wght@300;500;600&display=swap');
-body{
 
-  font-family: 'Inconsolata', monospace;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: center;
-  min-height: 100vh;
-  margin: 0px;
-  padding: 30px;
-}
+    body {
 
-/*
+        font-family: 'Inconsolata', monospace;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+        min-height: 100vh;
+        margin: 0px;
+        padding: 30px;
+    }
+
+    /*
 #experience {
     display: flex;
     flex-direction: row;
@@ -62,39 +63,39 @@ body{
         <hr style="border-width: 4px">
         <div id="personal">
             <h1>Hey, I'm Sammy</h1>
-            <h5> My goal is to bridge the gap between business and technology in Africa.</h5>
-            
+            <h5>I enjoy building and implementing the logic to meet business requirement.
+                My goal is to bridge the gap between business and technology in Africa.</h5>
+            <br>
         </div>
         <div id="skills">
             <!-- <h5 class="text-primary">Skills</h5> -->
             <p><span class="text-dark" style="font-weight: bolder; font-size: 20px">Skills:</span> Good knowledge of HTML, CSS, Bootstrap, JavaScript, WordPress, PHP, UNIT TESTING, GIT, OOP, Laravel, VueJs(Basic) , MYSQL and POSTGRE.</p>
         </div>
-        <div><p class=""> <a href="https://github.com/quitenoisemaker" ><img src="logo/github.png" width="100"></a>  <a href="https://linkedin.com/in/samsonojugo "><img src="logo/linkedin.png" width="100"></a> </p></div>
+        <div>
+            <p class=""> <a href="https://github.com/quitenoisemaker"><img src="logo/github.png" width="100"></a> <a href="https://linkedin.com/in/samsonojugo "><img src="logo/linkedin.png" width="100"></a> </p>
+        </div>
         <div id="skills">
             <h4 class="text-dark">Here are links to some of my works</h4>
             <ul style="list-style-type: none;">
-                <li><a href="https://mystore.techlybro.com/" class="text-danger">https://mystore.techlybro.com</a></li>
-                <li><a href="https://techlybro.com/" class="text-danger">https://techlybro.com</a></li>
-                <li><a href="https://swiftwheelslogistic.com/" class="text-danger">https://swiftwheelslogistic.com</a></li>
+                <li><a href="https://jand2gidi.com.ng/" class="text-danger">https://jand2gidi.com.ng</a></li>
+                <li><a href="https://kitchen.afrishoppi.com/" class="text-danger">https://kitchen.afrishoppi.com</a></li>
+                <li><a href="http://dotcityfm.com/" class="text-danger">http://dotcityfm.com</a></li>
                 <li><a href=" https://readerslogue.com.ng/" class="text-danger"> https://readerslogue.com.ng</a></li>
-                <li><a href="https://myschool.techlybro.com/online/" class="text-danger">https://myschool.techlybro.com/online</a></li>
                 <li><a href="https://kamilightglobal.com/" class="text-danger">https://kamilightglobal.com</a></li>
-                <li><a href="https://geniusgistmagazine.com/" class="text-danger">https://geniusgistmagazine.com</a></li>
-                
             </ul>
         </div>
-        
+
         <section id="contact">
-     
+
             <div class="text-center p-4">
                 <p><span style="font-weight: bolder; font-size: 20px" class="text-dark">Favourite Quote:</span> <q>The secret of life, though, is to fall seven times and to get up eight times.</q></p>
 
                 <button type="button" class="btn btn-outline-dark"><a href="mailto: samsonojugo@gmail.com" style="color: inherit; text-decoration: inherit;">
-                Contact</a></button>
+                        Contact</a></button>
             </div>
-           
-            
- 
+
+
+
             <hr style="border-width: 4px">
         </section>
     </div>
